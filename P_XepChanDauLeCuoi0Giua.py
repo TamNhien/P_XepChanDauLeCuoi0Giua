adbae7d53734590c09537f733581d07598e6b387
TamNhien <canhthien91@gmail.com>
2024-03-28 20:39:54 +0700

    Add project files.

---
+++ P_XepChanDauLeCuoi0Giua.py
@@ -0,0 +1,7 @@
+﻿L = list(map(int, input("Nhập vào danh sách các số nguyên, cách nhau bởi dấu phẩy: ").split(',')))
+
+# Sắp xếp các số chẵn lên đầu danh sách
+L.sort(key=lambda x: x % 2 == 0 and x != 0, reverse=True)
+
+print("Danh sách sau khi sắp xếp: ", L)
+
